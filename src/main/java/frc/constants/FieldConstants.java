02671cfb964fc47add83786c02f579e2cc9015a9
Luca Manolache <luca1230@gmail.com>
2022-12-12 10:58:38 -0800

    Update deps

--- src/main/java/frc/constants/FieldConstants.java
+++ src/main/java/frc/constants/FieldConstants.java
@@ -1,11 +1,9 @@
 package frc.constants;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.*;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.apriltag.AprilTag;
-import edu.wpi.first.wpilibj.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTag;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
 
 import java.util.List;
 
@@ -18,10 +16,35 @@ public class FieldConstants {
     public static final double ROBOT_MOI = 8.0;
     public static final double ROBOT_MASS = 60;
     public static final double ROBOT_WHEEL_RADIUS = Units.inchesToMeters(4);
-    public static final double WHEEL_DISTANCE = 0.55;
+    public static final double WHEEL_DISTANCE = 0.1;
+    public static final double TRACK_WIDTH = Units.inchesToMeters(32);
 
     public static List<AprilTag> APRIL_TAGS = List.of(
-            new AprilTag(0, new Pose3d())
+            // Upper Hub Blue-Near
+            new AprilTag(3,
+            new Pose3d(
+                    Units.inchesToMeters(332.321),
+                    Units.inchesToMeters(183.676),
+                    Units.inchesToMeters(95.186),
+                    new Rotation3d(0, Math.toRadians(26.75), Math.toRadians(69)))),
+
+            // Blue Terminal Near Station
+            new AprilTag(1,
+            new Pose3d(
+                    Units.inchesToMeters(4.768),
+                    Units.inchesToMeters(67.631),
+                    Units.inchesToMeters(35.063),
+                    new Rotation3d(0, 0, Math.toRadians(46.25)))
+            ),
+
+            // Blue Hangar Truss - Hub
+            new AprilTag(2,
+            new Pose3d(
+                    Units.inchesToMeters(127.272),
+                    Units.inchesToMeters(216.01),
+                    Units.inchesToMeters(67.932),
+                    new Rotation3d(0, 0, 0))
+            )
     );
     public static final AprilTagFieldLayout APRIL_FIELD = new AprilTagFieldLayout(APRIL_TAGS, FIELD_LENGTH, FIELD_WIDTH);
 }
--- src/main/java/frc/robot/RobotState.java
+++ src/main/java/frc/robot/RobotState.java
@@ -5,17 +5,22 @@ import edu.wpi.first.math.Nat;
 import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import frc.constants.FieldConstants;
 import org.littletonrobotics.junction.Logger;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
 public class RobotState {
 
     /* DRIVE */
-    public DifferentialDrivePoseEstimator drivePose = new DifferentialDrivePoseEstimator(new Rotation2d(), new Pose2d(),
-            new MatBuilder<>(Nat.N5(), Nat.N1()).fill(0.02, 0.02, 0.01, 0.02, 0.02), // State measurement standard deviations. X, Y, theta.
+    public DifferentialDriveKinematics driveKinematics = new DifferentialDriveKinematics(FieldConstants.TRACK_WIDTH);
+    public DifferentialDrivePoseEstimator drivePose = new DifferentialDrivePoseEstimator(driveKinematics,
+            new Rotation2d(), 0, 0, new Pose2d(),
             new MatBuilder<>(Nat.N3(), Nat.N1()).fill(0.02, 0.02, 0.01), // Local measurement standard deviations. Left encoder, right encoder, gyro.
             new MatBuilder<>(Nat.N3(), Nat.N1()).fill(0.1, 0.1, 0.01));
+    public DifferentialDriveOdometry drivePoseSim = new DifferentialDriveOdometry(new Rotation2d(), 0, 0, new Pose2d());
     public boolean gyroReady = false;
     public Rotation2d driveYaw;
     public double driveYawAngularVelocityDegrees = 0.0;
